ab9b02c0ba6127feb759005be60cf6b1d30af790
André Schenkels <andre@schenkels.nl>
2022-01-13 17:48:53 +0000

    empty module

---
+++ community/database_cleanup/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Database Cleanup",
+    'version': '15.0.0.1',
+    'category': 'Tools',
+    'license': 'Other proprietary',
+    'summary': """Test
+    """,
+    'author': "ERP-M",
+    'website': 'http://www.erp-m.eu',
+    'depends': ['base',],
+    'data': [
+    ],
+    'images':[
+    ],    
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
